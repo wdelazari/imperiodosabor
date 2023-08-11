1f051c3db455ca325097e4cf206aa5f466985dec
Winner Delazari <106100695+wdelazari@users.noreply.github.com>
2023-08-11 20:40:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ function closeModal() {
     var modal = document.querySelector('.modal');
     modal.style.display = 'none';
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const backToTopButton = document.getElementById("back-to-top");
+
+    backToTopButton.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
+
